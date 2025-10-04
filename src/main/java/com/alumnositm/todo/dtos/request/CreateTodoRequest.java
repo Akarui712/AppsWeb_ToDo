9846ea573f7b25b06ca5db0ecc88ae64d5ac2f9d
RoHe Fernanda <a21490574@itmexicali.edu.mx>
2025-10-03 19:59:51 -0700

    3_Oct_25

--- src/main/java/com/alumnositm/todo/dtos/request/CreateTodoRequest.java
+++ src/main/java/com/alumnositm/todo/dtos/request/CreateTodoRequest.java
@@ -1,12 +1,25 @@
 package com.alumnositm.todo.dtos.request;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Data
 public class CreateTodoRequest 
 {
+    //Se especifican las validaciones para cada variable
+    //Pueden tener los dos tipos o solo uno, por eso se ponen por separado
+    @NotBlank(message = "El título no puede estar vacío")
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9\\s\\-_.,!¡]+$", 
+            message = "El título no puede tener espacios al inicio o al final")
     private String title;
+
+    @NotBlank(message = "El título no puede estar vacío")
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9\\s\\-_.,!¡]+$", 
+            message = "La descripción no puede tener espacios al inicio o al final")
     private String description;
 
     public CreateTodoRequest(String title, String description)
@@ -14,6 +27,4 @@ public class CreateTodoRequest
         this.title = title;
         this.description = description;
     }
-
-
 }
